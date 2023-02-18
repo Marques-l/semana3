f9a361d98fa84504eefe1c33b70da9cd12543e4c
larissa <larissamarques.ufam@gmail.com>
2023-02-18 09:28:34 -0300

    instancia carro

--- Carro.cs
+++ Carro.cs
@@ -12,5 +12,8 @@ namespace semana3
         public string Cor { get; set; }
         public string Marca { get; set; }
         
+       public void CadastrarCarro() {
+
+       }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-using semana3; 
+﻿using semana3; 
 string opcao; 
 List<Carro> Ticket = new List<Carro>();  
+ Estaciomento NovaEntrada = new Estaciomento();
+ Carro NovoCarro = new Carro(); 
 do{
 Console.WriteLine("1-Cadastrar carro");
 Console.WriteLine("2-Marcar entrada");
@@ -8,23 +10,26 @@ Console.WriteLine("3-Marcar saída");
 Console.WriteLine("4-Consultar histórico");
 Console.WriteLine("5-sair");
 opcao = Console.ReadLine(); 
-if (opcao=="1"){
 
+if (opcao=="1"){
+ Console.WriteLine("Insira a placa do veículo"); 
+}
+if(opcao =="2") { 
+Console.WriteLine("Hora de entrada"); 
+NovaEntrada.Entrada = DateTime.Parse(Console.ReadLine()); 
 }
 if (opcao=="3"){
-
+Console.WriteLine("Hora de saída");
+NovaEntrada.Saida=DateTime.Parse(Console.ReadLine()); 
 }
+
 if (opcao=="4"){
 
 }
+
 if (opcao=="5"){
 
 }
 } while (opcao!="3"); 
 Console.WriteLine("Tecle Enter para continuar"); 
 Console.ReadLine(); 
-//Estaciomento NovaEntrada = new Estaciomento(); 
-//Console.WriteLine("Hora de entrada"); 
-//NovaEntrada.Entrada = DateTime.Parse(Console.ReadLine()); 
-//Console.WriteLine("Hora de saída");
-//NovaEntrada.Saida=DateTime.Parse(Console.ReadLine()); 
