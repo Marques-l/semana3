3a9da428e20abe29562970c20fcc8e386954d879
larissa <larissamarques.ufam@gmail.com>
2023-02-18 06:56:18 -0300

    Criação do menu

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
 ﻿using semana3; 
+string opcao; 
 List<Carro> Ticket = new List<Carro>();  
+do{
+Console.WriteLine("1-Cadastrar carro");
+Console.WriteLine("2-Marcar entrada");
+Console.WriteLine("3-Marcar saída");
+Console.WriteLine("4-Consultar histórico");
+Console.WriteLine("5-sair");
+opcao = Console.ReadLine(); 
+} while (opcao!="3"); 
 Estaciomento NovaEntrada = new Estaciomento(); 
 Console.WriteLine("Hora de entrada"); 
 NovaEntrada.Entrada = DateTime.Parse(Console.ReadLine()); 
