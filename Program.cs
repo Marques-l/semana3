996af3131719b72e5113ee5a1787dbdf122f0d7f
larissa <larissamarques.ufam@gmail.com>
2023-02-18 07:43:03 -0300

    Opções do menu

--- Program.cs
+++ Program.cs
@@ -8,9 +8,26 @@ Console.WriteLine("3-Marcar saída");
 Console.WriteLine("4-Consultar histórico");
 Console.WriteLine("5-sair");
 opcao = Console.ReadLine(); 
+if (opcao=="1"){
+
+}
+if (opcao=="2"){
+
+}
+if (opcao=="3"){
+
+}
+if (opcao=="4"){
+
+}
+if (opcao=="5"){
+
+}
 } while (opcao!="3"); 
-Estaciomento NovaEntrada = new Estaciomento(); 
-Console.WriteLine("Hora de entrada"); 
-NovaEntrada.Entrada = DateTime.Parse(Console.ReadLine()); 
-Console.WriteLine("Hora de saída");
-NovaEntrada.Saida=DateTime.Parse(Console.ReadLine()); 
+Console.WriteLine("Tecle Enter para continuar"); 
+Console.ReadLine(); 
+//Estaciomento NovaEntrada = new Estaciomento(); 
+//Console.WriteLine("Hora de entrada"); 
+//NovaEntrada.Entrada = DateTime.Parse(Console.ReadLine()); 
+//Console.WriteLine("Hora de saída");
+//NovaEntrada.Saida=DateTime.Parse(Console.ReadLine()); 
