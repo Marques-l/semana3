9987776be64d416fcdeb2f2442ee9e6cad9ebeba
larissa <larissamarques.ufam@gmail.com>
2023-03-13 09:06:25 -0300

    Corrigindo as listas

--- Carro.cs
+++ Carro.cs
@@ -11,15 +11,15 @@ namespace semana3
         public string Modelo { get; set; }
         public string Cor { get; set; }
         public string Marca { get; set; }
-         
-         public  List<Ticket> NovaEntrada {get; set;} 
-         public  List<Ticket> NovaSaida {get; set;}
+         public List<Ticket> Tickets {get; set;} // Essa vai ser a lista que provavelmente vai ser usada
+         public  List<Ticket> NovaEntrada {get; set;} //Provavelmente tirar isso depois 
+         public  List<Ticket> NovaSaida {get; set;} //Provavelmente tirar isso depois
         public static List<Carro> Carros1 = new List<Carro>(); 
          
            public Carro(){
-            
-          NovaEntrada = new List<Ticket>(); 
-          NovaSaida = new List<Ticket>(); 
+            Tickets = new List<Ticket>(); 
+            NovaEntrada = new List<Ticket>(); //Isso provavelmente vai sair também. 
+            NovaSaida = new List<Ticket>();  //Isso provavelmente vai sair também. 
           
         }
         public Carro(string placa, string modelo, string cor, string marca) :this()
